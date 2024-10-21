f82f84d10efd4b0a3ef4c804de741040116d11dc
egorka13 <shir-egor@yandex.ru>
2024-10-22 00:19:52 +0200

    remove prohibited package

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,12 @@ const path = require('path');
 module.exports = {
   target: 'node',
   mode: 'production',
-  entry: './src/index.ts',
+  entry: './dist/sourceMap/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+    extensions: ['.js'],
   },
 };
